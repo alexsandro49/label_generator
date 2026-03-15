f9f5e3f3ab7e80be880820bb3069b5eb1af388ad
Alex Sandro <alexsandro49.dev@gmail.com>
2026-03-15 11:13:01 -0300

    feat: header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<template>
+  <header class="bg-deep-navy w-full h-[10vh] px-5 flex items-center justify-between">
+    <h1 class="text-white text-2xl font-bold">
+      Gerenciador de etiquetas
+    </h1>
+    <FontAwesomeIcon
+      class="text-white text-xl cursor-pointer"
+      :icon="faMagnifyingGlass"
+    />
+  </header>
+</template>
