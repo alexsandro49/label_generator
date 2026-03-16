266a6c25a2b6cfe186650c33988f076770d020e7
Alex Sandro <alexsandro49.dev@gmail.com>
2026-03-16 19:31:57 -0300

    fix: vite config

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -4,11 +4,8 @@ export function convertMmToPt(value: number) {
   return (value * 72) / 25.4;
 }
 
-const api_url = import.meta.env.VITE_API_URL;
-const port = import.meta.env.VITE_PORT;
-
 const api = axios.create({
-  baseURL: `${api_url}:${port}`,
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000',
 });
 
 export default api;
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/label-generator/',
   plugins: [vue(), tailwindcss()],
   server: {
     port: 1573
